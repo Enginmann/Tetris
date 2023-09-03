3c8a4969ab52700ab852e94783f3c0b2adf16b4c
Omar <oa2182005@gmail.com>
2023-09-03 17:57:41 +0300

    Finally!!🎉🎉 the game is playable, but it contains some bugs 🐞 which don't affect the gameplay.

--- tetris.py
+++ tetris.py
@@ -1,9 +1,13 @@
+# This code works.
+# But it contains some bugs.
+# However, it they don't affect the gameplay.
+
 import random
 from sys import exit
 import numpy as np
 import pygame as pg
 import json
-import os
+from os import path
 
 
 class Grid():
@@ -21,18 +25,24 @@ class Grid():
 
     def display_score(self):
         score_surf = font.render(f'Score: {self.score}', False, grey)
-        score_rect = score_surf.get_rect(left = 20, top = self.top_panal_height / 2 - 20)
+        if game_over:
+            score_rect = score_surf.get_rect(center=(int(screen_width/2), self.top_panal_height-30))
+        else:
+            score_rect = score_surf.get_rect(left=20, top=int(self.top_panal_height/2)-20)
         screen.blit(score_surf, score_rect)
-    
+
     def display_highest_score(self, highest_score):
         highest_score_surf = font.render(f'Highest score: {highest_score}', False, grey)
-        highest_score_rect = highest_score_surf.get_rect(left = 20, top = self.top_panal_height / 2 - 20)
+        if starting:
+            highest_score_rect = highest_score_surf.get_rect(center=(int(screen_width/2), int(self.top_panal_height/2)))
+        else:
+            highest_score_rect = highest_score_surf.get_rect(center=(int(screen_width/2), self.top_panal_height-70))
         screen.blit(highest_score_surf, highest_score_rect)
-    
+
     def display_next_block(self, next_block, next_color):
         side_length = 20
         text_surf = font.render('Next: ', False, grey)
-        text_rect = text_surf.get_rect(left = screen_width - 9 * side_length, top = self.top_panal_height / 2 - 20)
+        text_rect = text_surf.get_rect(left=screen_width - 9 * side_length, top=int(self.top_panal_height / 2) - 20)
         screen.blit(text_surf, text_rect)
         next_block_rows, next_block_cols = next_block.shape
         horizontal_margin = side_length * (next_block_cols) + side_length
@@ -40,7 +50,7 @@ class Grid():
         for j in range(next_block_rows):
             for i in range(next_block_cols):
                 if next_block[j][i] == 1:
-                    pg.draw.rect(screen, next_color, [i * side_length + screen_width - horizontal_margin , j * side_length + vertical_margin, side_length, side_length])
+                    pg.draw.rect(screen, next_color, [i * side_length + screen_width - horizontal_margin, j * side_length + vertical_margin, side_length, side_length])
 
     def display_grid(self):
         for j in range(self.cols + 1):
@@ -49,11 +59,13 @@ class Grid():
             pg.draw.line(screen, self.color, (0, i * self.y_offset + self.top_panal_height), (self.grid_width, i * self.y_offset + self.top_panal_height), self.line_width)
         pg.draw.rect(screen, self.color, [0, 0, screen_width, self.top_panal_height])
 
-    def display_block(self, block_grid, color):
-        for j in range(grid.rows):
-            for i in range(grid.cols):
-                if self.grid[j][i] == 1:
-                    pg.draw.rect(screen, color, [i * self.x_offset, j * self.y_offset + self.top_panal_height, self.x_offset, self.y_offset])
+    def display_block(self, block):
+        indices = np.where(block.grid == 1)
+        for i in range(len(indices[0])):
+            if len(indices[1]) > 0:
+                pg.draw.rect(screen, block.color, [indices[1][i] * self.x_offset, indices[0][i] * self.y_offset + self.top_panal_height, self.x_offset, self.y_offset])
+            else:
+                pg.draw.rect(screen, block.color, [block.x_pos * self.x_offset, indices[0][i] * self.y_offset + self.top_panal_height, self.x_offset, self.y_offset])
 
     def display_shadow_block(self, block):
         shadow_color = 0
@@ -69,11 +81,11 @@ class Grid():
             shadow_color = shadow_orange
         elif block.color == purple:
             shadow_color = shadow_purple
-        shadow_grid = grid.grid[0 : grid.rows, block.x_pos : block.x_pos + block.shape_cols].copy()
+        shadow_grid = grid.grid[0: grid.rows, block.x_pos: block.x_pos + block.shape_cols].copy()
         shadow_grid_rows, shadow_grid_cols = shadow_grid.shape
         shadow_block_y_pos = block.y_pos
         shadow_block_x_pos = block.x_pos
-        shadow_grid[0 : shadow_block_y_pos + block.shape_rows, 0 : block.shape_cols] *= 0
+        shadow_grid[0: shadow_block_y_pos + block.shape_rows, 0: block.shape_cols] *= 0
         indecies = np.where(shadow_grid == 1)
         shape_indecies = []
         shape_j_indecies = np.where(block.shape == 1)[0]
@@ -96,7 +108,7 @@ class Grid():
                 y_pos += 1
         else:
             shadow_block_y_pos = grid.rows - block.shape_rows
-        shadow_grid[shadow_block_y_pos : shadow_block_y_pos + block.shape_rows, :] += block.shape
+        shadow_grid[shadow_block_y_pos: shadow_block_y_pos + block.shape_rows, :] += block.shape
         for j in range(shadow_grid_rows):
             for i in range(shadow_grid_cols):
                 if shadow_grid[j][i] == 1:
@@ -156,12 +168,13 @@ class Grid():
     def reset_score(self):
         self.score = 0
 
+
 class Block():
-    def __init__(self, next_block, next_color):
+    def __init__(self, next_block, next_color, wait_time):
         self.shape = next_block
         self.color = next_color
         self.stopped = False
-        self.wait_time = 0.8
+        self.wait_time = wait_time
         self.shape_rows, self.shape_cols = self.shape.shape
         self.x_pos = int(grid.cols / 2) - 1
         self.y_pos = 0
@@ -172,8 +185,8 @@ class Block():
         self.grid = np.zeros((grid.rows, grid.cols))
         self.add_block_new_pos()
 
-    def add_new_block(self, next_block, next_color):
-        new_block = Block(next_block, next_color)
+    def add_new_block(self, next_block, next_color, wait_time):
+        new_block = Block(next_block, next_color, wait_time)
         blocks.append(new_block)
 
     def stop_block(self):
@@ -181,21 +194,25 @@ class Block():
         self.right = False
         self.left = False
         self.stopped = True
-    
+
     def update_block_shape(self):
         self.shape_rows, self.shape_cols = self.shape.shape
 
     def add_block_new_pos(self):
         indecies = np.where(self.shape == 1)
         for i in range(len(indecies[0])):
-            grid.grid[indecies[0][i] + self.y_pos][indecies[1][i] + self.x_pos] = 1
-            self.grid[indecies[0][i] + self.y_pos][indecies[1][i] + self.x_pos] = 1
+            grid.grid[indecies[0][i] +
+                      self.y_pos][indecies[1][i] + self.x_pos] = 1
+            self.grid[indecies[0][i] +
+                      self.y_pos][indecies[1][i] + self.x_pos] = 1
 
     def delete_block_past_pos(self):
         indecies = np.where(self.shape == 1)
         for i in range(len(indecies[0])):
-            grid.grid[indecies[0][i] + self.y_pos][indecies[1][i] + self.x_pos] = 0
-            self.grid[indecies[0][i] + self.y_pos][indecies[1][i] + self.x_pos] = 0
+            grid.grid[indecies[0][i] +
+                      self.y_pos][indecies[1][i] + self.x_pos] = 0
+            self.grid[indecies[0][i] +
+                      self.y_pos][indecies[1][i] + self.x_pos] = 0
 
     def check_adjacent_blocks(self):
         if self.stopped:
@@ -213,7 +230,7 @@ class Block():
             mask_cols_end_index = mask_cols_limit_index
         if mask_cols_start_index < 0:
             mask_cols_start_index = 0
-        mask = grid.grid[mask_rows_start_index : mask_rows_end_index, mask_cols_start_index : mask_cols_end_index].copy()
+        mask = grid.grid[mask_rows_start_index: mask_rows_end_index, mask_cols_start_index: mask_cols_end_index].copy()
         mask_rows, mask_cols = mask.shape
         self.down = True
         self.right = True
@@ -246,6 +263,25 @@ class Block():
         if not self.down:
             self.stopped = True
 
+    def update_block_grid(self, block, grid_copy):
+        completed_rows_indecies = []
+        completed_row = np.ones(grid.cols)
+        block_grid_copy = block.grid.copy()
+        for row in range(grid.rows):
+            if (grid_copy[row] == completed_row).all():
+                block_grid_copy[row] *= 0
+                completed_rows_indecies.append(row)
+        completed_rows_num = len(completed_rows_indecies)
+        if completed_rows_num == 0:
+            return
+        for i in range(grid.rows):
+            if (completed_rows_num - 1) < i <= completed_rows_indecies[-1]:
+                block.grid[i] = block_grid_copy[i - completed_rows_num]
+            elif i > completed_rows_indecies[-1]:
+                block.grid[i] = block_grid_copy[i]
+            else:
+                block.grid[i] = np.zeros(grid.cols)
+
     def gravity(self):
         if self.stopped:
             return
@@ -256,8 +292,8 @@ class Block():
 
     def fall_down(self):
         self.delete_block_past_pos()
-        grid_copy = grid.grid[0 : grid.rows, self.x_pos : self.x_pos + self.shape_cols].copy()
-        grid_copy[0 : self.y_pos, self.x_pos : self.x_pos + self.shape_cols] *= 0
+        grid_copy = grid.grid[0: grid.rows, self.x_pos: self.x_pos + self.shape_cols].copy()
+        grid_copy[0: self.y_pos, self.x_pos: self.x_pos + self.shape_cols] *= 0
         indecies = np.where(grid_copy == 1)
         shape_indecies = []
         shape_j_indecies = np.where(self.shape == 1)[0]
@@ -284,23 +320,9 @@ class Block():
 
     def rotate_block(self):
         self.delete_block_past_pos()
-        # lst1 = np.where(grid.grid[self.y_pos] == 1)[0]
-        # lst2 = np.where(grid.grid[self.y_pos:grid.rows, self.x_pos:self.x_pos+self.shape_cols] == 1)[1]
         if self.shape_rows + self.x_pos > grid.cols or self.shape_cols + self.y_pos > grid.rows:
             self.add_block_new_pos()
             return
-        # if self.x_pos + self.shape_rows - 1 >= lst1.all() and len(lst1) > 0:
-        #     self.add_block_new_pos()
-        #     return
-        # if self.y_pos + self.shape_cols - 1 >= lst2.all() and len(lst2) > 0:
-        #     self.add_block_new_pos()
-        #     return
-        # if grid.grid[self.y_pos][self.x_pos + self.shape_rows] == 1:
-        #     self.add_block_new_pos()
-        #     return
-        # if grid.grid[self.y_pos + self.shape_cols][self.x_pos] == 1:
-        #     self.add_block_new_pos()
-        #     return
         self.shape = np.rot90(self.shape, 1)
         self.update_block_shape()
         self.add_block_new_pos()
@@ -318,22 +340,43 @@ class Block():
     def move_block_down(self):
         if self.y_pos + self.shape_rows < grid.rows:
             self.y_pos += 1
-            
+
     def move_block_right(self):
         if self.x_pos + self.shape_cols < grid.cols:
             self.x_pos += 1
-            
+
     def move_block_left(self):
         if self.x_pos > 0:
             self.x_pos -= 1
+
+
+def increase_difficulity():
+    global wait_time
+    if grid.score >= 6000:
+        wait_time = 0.3
+    elif grid.score >= 5000:
+        wait_time = 0.35
+    elif grid.score >= 4500:
+        wait_time = 0.4
+    elif grid.score >= 3000:
+        wait_time = 0.5
+    elif grid.score >= 2000:
+        wait_time = 0.65
+    elif grid.score >= 1000:
+        wait_time = 0.75
+    elif grid.score >= 500:
+        wait_time = 0.8
+
 
 def gameover():
     global playing, game_over
     playing = False
     game_over = True
 
+
 def restart():
     global blocks
+    wait_time = 0.9
     next_block = grid.next_block()
     next_color = grid.next_color()
     for block in blocks:
@@ -341,13 +384,14 @@ def restart():
     grid.reset_grid()
     grid.reset_score()
     blocks = []
-    blocks.append(Block(next_block, next_color))
+    blocks.append(Block(next_block, next_color, wait_time))
     next_block = grid.next_block()
     next_color = grid.next_color()
 
+
 def update_highest_score():
     global highest_score
-    if not os.path.exists('highest_score.json'):
+    if not path.exists('highest_score.json'):
         with open('highest_score.json', 'w') as f:
             json.dump(0, f)
     with open('highest_score.json', 'r') as f:
@@ -360,16 +404,17 @@ def update_highest_score():
         with open('highest_score.json', 'r') as f:
             highest_score = json.load(f)
 
+
 pg.init()
-screen_width = 370
-screen_height = 2*screen_width+100
+screen_width = 400
+screen_height = 2 * screen_width + 100
 cols = 10
 screen = pg.display.set_mode((screen_width, screen_height))
 pg.display.set_caption('Tetris')
 clock = pg.time.Clock()
 fps = 60
 font_size = 25
-font = pg.font.Font('Fonts/Roboto-Bold.ttf', font_size)
+font = pg.font.Font('E:/Coding Projects/Python Projects/Tetris/Fonts/Roboto-Bold.ttf', font_size)
 
 # Colors
 white = (250, 250, 250)
@@ -389,10 +434,13 @@ grey = (216, 216, 216)
 black = (0, 0, 0)
 bg_color = white
 grid_color = black
+
+# Initailizing variables
 grid = Grid(cols, grid_color)
 next_block = grid.next_block()
 next_color = grid.next_color()
-first_block = Block(next_block, next_color)
+wait_time = 0.9
+first_block = Block(next_block, next_color, wait_time)
 blocks = [first_block]
 next_block = grid.next_block()
 next_color = grid.next_color()
@@ -403,50 +451,54 @@ game_over = False
 
 while True:
     for event in pg.event.get():
-            if event.type == pg.QUIT:
+        if event.type == pg.QUIT:
+            pg.quit()
+            exit()
+        if event.type == pg.KEYDOWN:
+            if playing:
+                if event.key == pg.K_DOWN:
+                    blocks[-1].fall_down()
+                elif event.key == pg.K_UP:
+                    blocks[-1].rotate_block()
+                elif event.key == pg.K_RIGHT:
+                    blocks[-1].move_block('right')
+                elif event.key == pg.K_LEFT:
+                    blocks[-1].move_block('left')
+                elif event.key == pg.K_SPACE:
+                    paused = True
+                    playing = False
+            elif paused:
+                if event.key == pg.K_SPACE:
+                    paused = False
+                    playing = True
+            else:
+                if event.key == pg.K_RETURN:
+                    playing = True
+                    game_over = False
+                    starting = False
+                    next_block = grid.next_block()
+                    next_color = grid.next_color()
+                    restart()
+            if event.key == pg.K_ESCAPE:
                 pg.quit()
                 exit()
-            if event.type == pg.KEYDOWN:
-                if playing:
-                    if event.key == pg.K_DOWN:
-                        blocks[-1].fall_down()
-                    elif event.key == pg.K_UP:
-                        blocks[-1].rotate_block()
-                    elif event.key == pg.K_RIGHT:
-                        blocks[-1].move_block('right')
-                    elif event.key == pg.K_LEFT:
-                        blocks[-1].move_block('left')
-                    elif event.key == pg.K_SPACE:
-                        paused = True
-                        playing = False
-                elif paused:
-                    if event.key == pg.K_SPACE:
-                        paused = False
-                        playing = True
-                else:
-                    if event.key == pg.K_RETURN:
-                        playing = True
-                        game_over = False
-                        starting = False
-                        next_block = grid.next_block()
-                        next_color = grid.next_color()
-                        restart()
-                if event.key == pg.K_ESCAPE:
-                    pg.quit()
-                    exit()
     if playing:
         screen.fill(bg_color)
         grid.display_shadow_block(blocks[-1])
         blocks[-1].check_adjacent_blocks()
         blocks[-1].gravity()
+        grid_copy = grid.grid.copy()
         if blocks[-1].stopped:
             blocks[-1].stop_block()
             grid.erase_completed_rows()
-            blocks[-1].add_new_block(next_block, next_color)
+            increase_difficulity()
+            blocks[-1].add_new_block(next_block, next_color, wait_time)
             next_block = grid.next_block()
             next_color = grid.next_color()
         for block in blocks:
-            grid.display_block(block.grid, block.color)
+            # if len(blocks) > 0:
+            block.update_block_grid(block, grid_copy)
+            grid.display_block(block)
         grid.display_grid()
         grid.display_score()
         grid.display_next_block(next_block, next_color)
@@ -456,15 +508,15 @@ while True:
         screen.fill(bg_color)
         grid.display_shadow_block(blocks[-1])
         for block in blocks:
-            grid.display_block(block.grid, block.color)
+            grid.display_block(block)
         grid.display_grid()
         grid.display_score()
         grid.display_next_block(next_block, next_color)
         pg.draw.rect(screen, black, [0, int(screen_height/2) - 50, screen_width, 100])
         text_surf_1 = font.render('Paused', False, red)
-        text_rect_1 = text_surf_1.get_rect(center = ((int(screen_width/2)), int(screen_height/2)-20))
+        text_rect_1 = text_surf_1.get_rect(center=((int(screen_width/2)), int(screen_height/2)-20))
         text_surf_2 = font.render('press Space to resume', False, green)
-        text_rect_2 = text_surf_2.get_rect(center = ((int(screen_width/2)), int(screen_height/2)+20))
+        text_rect_2 = text_surf_2.get_rect(center=((int(screen_width/2)), int(screen_height/2)+20))
         screen.blit(text_surf_1, text_rect_1)
         screen.blit(text_surf_2, text_rect_2)
         pg.display.flip()
@@ -473,14 +525,15 @@ while True:
         update_highest_score()
         screen.fill(bg_color)
         for block in blocks:
-            grid.display_block(block.grid, block.color)
+            grid.display_block(block)
         grid.display_grid()
         grid.display_highest_score(highest_score)
+        grid.display_score()
         pg.draw.rect(screen, black, [0, int(screen_height/2) - 50, screen_width, 100])
         game_over_text_surf = font.render('Game Over', False, red)
-        game_over_text_rect = game_over_text_surf.get_rect(center = ((int(screen_width/2)), int(screen_height/2)-20))
+        game_over_text_rect = game_over_text_surf.get_rect(center=((int(screen_width/2)), int(screen_height/2)-20))
         play_again_text_surf = font.render('press Enter to play again', False, green)
-        play_again_text_rect = play_again_text_surf.get_rect(center = ((int(screen_width/2)), int(screen_height/2)+20))
+        play_again_text_rect = play_again_text_surf.get_rect(center=((int(screen_width/2)), int(screen_height/2)+20))
         screen.blit(game_over_text_surf, game_over_text_rect)
         screen.blit(play_again_text_surf, play_again_text_rect)
         pg.display.flip()
@@ -492,7 +545,7 @@ while True:
         grid.display_highest_score(highest_score)
         pg.draw.rect(screen, black, [0, int(screen_height/2) - 50, screen_width, 100])
         text_surf = font.render('Press Enter to Play', False, light_blue)
-        text_rect = text_surf.get_rect(center = ((int(screen_width/2)), int(screen_height/2)))
+        text_rect = text_surf.get_rect(center=((int(screen_width/2)), int(screen_height/2)))
         screen.blit(text_surf, text_rect)
         pg.display.flip()
         clock.tick(fps)
